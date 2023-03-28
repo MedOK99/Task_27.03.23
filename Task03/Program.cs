6e86176bc71f354dee0fc9ae2f44320430be5a30
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-28 12:43:13 +0300

    готово выходные

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -5,3 +5,51 @@
 7 -> да
 1 -> нет  */
 
+Console.Clear();
+
+Console.WriteLine("Введите число:  ");
+
+string day = Console.ReadLine();
+
+int d = int.Parse(day);
+
+if (d == 1)
+{
+  Console.WriteLine("НЕТ");
+}
+
+if (d == 2)
+{
+  Console.WriteLine("НЕТ");
+}
+
+if (d == 3)
+{
+  Console.WriteLine("НЕТ");
+}
+
+if (d == 4)
+{
+  Console.WriteLine("НЕТ");
+}
+
+if (d == 5)
+{
+  Console.WriteLine("НЕТ");
+}
+
+if (d == 6)
+{
+  Console.WriteLine("ДА");
+}
+
+if (d == 7)
+{
+  Console.WriteLine("ДА");
+}
+
+else
+{
+  if(d > 7)
+  Console.WriteLine("Нет такого дня недели");
+}
